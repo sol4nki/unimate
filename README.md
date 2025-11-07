d43e2ff5118ceba522ae7ff5ca3a60a7b96c675e
sol4nki <solankipranjal44@gmail.com>
2025-11-07 19:04:13 +0530

    settings change

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # Unimate
 prototype
 <!--test commit-->
+
+
+commands 
+
+
+
+make sure you have git -> https://git-scm.com/install/
+
+
+
+git clone https://github.com/sol4nki/unimate
+
+
+
+cd unimate
+
+
+
+node? 
+
+
+check by running -> node -v
+
+if no node -> google nodejs download -> link -> https://nodejs.org/en/download/
+
+re run
+
+
+node -v
+
+
+
+download expo go from playstore or apple store 
+
--- pages/Settings.js
+++ pages/Settings.js
@@ -8,7 +8,11 @@ export default function SettingsPage() {
     <ScrollView marginBottom={80}>
     <SafeAreaView style={{ flex: 1, paddingLeft: 20, paddingRight: 20, marginTop: 20, gap: 20 }}>
       {/* <Text style={styles.header}>Settings</Text> */}
-
+      <View style={styles.section} style={{position: 'absolute', top: 0, left: 0, backgroundColor: 'lightgrey', height: 160, width: '120%'}}/>
+      <View style={styles.item} style={{height: 100, width: 100, backgroundColor: 'gray', borderRadius: 500, alignSelf: 'center', zIndex: 900, marginTop: 60}}/>
+      <Text style={{ fontSize: 22, fontWeight: '600', alignSelf: 'center' }}>Pranjal Solanki</Text>
+      <Text style={{ fontSize: 16, fontWeight: '500', color: '#555555', alignSelf: 'center', marginTop: -15 }}>2025999</Text>
+      <Text style={{ fontSize: 16, fontWeight: '500', color: '#555555', alignSelf: 'center', marginTop: -15 }}>pranjal2599@iiitd.ac.in</Text>
       <View style={styles.section}>
         
       </View>
@@ -16,7 +20,10 @@ export default function SettingsPage() {
       <View style={styles.item} />
       <View style={styles.item} />
       <View style={styles.item} />
-      <View style={styles.item} />
+      <View style={styles.logout}>
+        <Text style={{ color: 'white', fontWeight: '800', fontSize: 16 }}>Logout</Text>
+      </View>
+
     </SafeAreaView>
     </ScrollView>
   );
@@ -45,4 +52,13 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginBottom: 15,
   },
+  logout: {
+    height: 50,
+    borderRadius: 10,
+    // borderWidth: 1,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+  }
 });
